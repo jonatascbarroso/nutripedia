d9de23eee3aa252154e5460e2310aceaa9edf938
Jonatas <jonatas@gmx.net>
2019-11-23 07:56:26 -0300

    updating

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   state = {
     metadata: [],
     data: []
-  }
+  };
 
   localStoredDataKey = '@nutripedia/data';
   localStoredLastUpdateKey = '@nutripedia/lastUpdate';
@@ -44,43 +44,59 @@ class App extends Component {
 
   checkLastUpdate() {
     let storedlastUpdate = localStorage.getItem(this.localStoredLastUpdateKey);
-    if (storedlastUpdate == null || storedlastUpdate === 'undefined') {
+    if (storedlastUpdate == null || typeof storedlastUpdate === 'undefined') {
       this.updateLocalStorage();
     } else {
       let localDate = new Date(storedlastUpdate);
       let remoteDate = new Date(this.state.metadata.lastUpdate);
-      if (localDate < remoteDate) {
+      if (localDate <= remoteDate) {
         this.updateLocalStorage();
       }
     }
+  }
+
+  createCard(food) {
+    let label = this.state.metadata;
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title">{food.name}</h3>
+          <img src={food.image} alt={food.image} className="img-thumbnail" />
+          <h4 className="card-subtitle mb-2">{label.properties}</h4>
+          <h5 className="card-subtitle mb-2 text-muted">{food.properties}</h5>
+          <h4 className="card-subtitle mb-2">{label.benefits}</h4>
+          <h5 className="card-subtitle mb-2 text-muted">{food.benefits}</h5>
+          <h4 className="card-subtitle mb-2">{label.composition}</h4>
+          <h5 className="card-subtitle mb-2 text-muted">{food.composition}</h5>
+          <h4 className="card-subtitle mb-2">{label.action}</h4>
+          <h5 className="card-subtitle mb-2 text-muted">{food.action}</h5>
+          <h4 className="card-subtitle mb-2">{label.nutrients}</h4>
+          <h5 className="card-subtitle mb-2 text-muted">{food.nutrients}</h5>
+          <h4 className="card-subtitle mb-2">{label.dailyPortion}</h4>
+          <h5 className="card-subtitle mb-2 text-muted">{food.dailyPortion}</h5>
+        </div>
+      </div>);
+  }
+
+  getContent() {
+    let content = '';
+    let collection = JSON.parse(localStorage.getItem(this.localStoredDataKey));
+    if (collection == null ||
+        typeof collection === 'undefined' ||
+        typeof collection.data === 'undefined') {
+      content = <h5 className="card-subtitle mb-2 text-muted">Food not found.</h5>;
+    } else {
+      for(let food in Object.values(collection.data)) {
+        content += this.createCard(food);
+      }
+    }
+    return content;
   }
   
   render() {
     this.updateMetadata();
     this.checkLastUpdate();
-    let content = <h5 className="card-subtitle mb-2 text-muted">Food not found.</h5>
-    let collection = JSON.parse(localStorage.getItem(this.localStoredDataKey));
-    for (let [key, food] of Object.entries(collection)) {
-      content =
-        <div className="card">
-          <div className="card-body">
-            <h3 className="card-title">{food.name}</h3>
-            <img src={food.image} alt={food.image} className="img-thumbnail" />
-            <h4 className="card-subtitle mb-2">{this.state.metadata.properties}</h4>
-            <h5 className="card-subtitle mb-2 text-muted">{food.properties}</h5>
-            <h4 className="card-subtitle mb-2">{this.state.metadata.benefits}</h4>
-            <h5 className="card-subtitle mb-2 text-muted">{food.benefits}</h5>
-            <h4 className="card-subtitle mb-2">{this.state.metadata.composition}</h4>
-            <h5 className="card-subtitle mb-2 text-muted">{food.composition}</h5>
-            <h4 className="card-subtitle mb-2">{this.state.metadata.action}</h4>
-            <h5 className="card-subtitle mb-2 text-muted">{food.action}</h5>
-            <h4 className="card-subtitle mb-2">{this.state.metadata.nutrients}</h4>
-            <h5 className="card-subtitle mb-2 text-muted">{food.nutrients}</h5>
-            <h4 className="card-subtitle mb-2">{this.state.metadata.dailyPortion}</h4>
-            <h5 className="card-subtitle mb-2 text-muted">{food.dailyPortion}</h5>
-          </div>
-        </div>
-    }
+    let content = this.getContent();
     return (
       <div className="container">
         <div className="col-xs-12">
